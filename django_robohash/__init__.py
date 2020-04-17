4de9e7ac61b7f04352276f608f5039a10bbf528d
Eric Lapouyade <elapouya@gmail.com>
2020-04-18 00:09:21 +0200

    update README

--- django_robohash/__init__.py
+++ django_robohash/__init__.py
@@ -1,6 +1,6 @@
-__version__='0.9.4'
+__version__='0.9.5'
 __author__ = "Eric Lapouyade"
-__copyright__ = "Copyright 2018, The Django listing Project"
+__copyright__ = "Copyright 2020, The Django Robohash SVG project"
 __credits__ = ["Eric Lapouyade"]
 __license__ = "LGPL v2.1"
 __maintainer__ = "Eric Lapouyade"
